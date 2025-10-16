9ddaf7ebf1b095b37e66021ac61f0af2a66de2c0
mypop <mypop@localhost>
2025-10-16 18:14:11 -0500

    Comment out pm2 startup command in foundryOracle.sh for clarity

--- scripts/foundryOracle.sh
+++ scripts/foundryOracle.sh
@@ -15,7 +15,7 @@ echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.co
 sudo apt update && sudo apt -y install nodejs
 sudo npm i -g pm2
 pm2 update
-pm2 startup
+# pm2 startup
 sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
 
 # ==== Create needed directories ====
